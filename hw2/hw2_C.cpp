2760df5f8b9bdf0f2de9d87ac18862265b607018
Seann <seanyu0425@gmail.com>
2024-09-19 19:46:57 +0800

    another progress

--- hw2/hw2_C.cpp
+++ hw2/hw2_C.cpp
@@ -5,7 +5,7 @@ typedef pair<int64_t, int64_t> Point;
 inline bool cross_product(const Point &a, const Point &b, const Point &c) {
     auto u_x = b.first - a.first, u_y = b.second - a.second;
     auto v_x = b.first - c.first, v_y = b.second - c.second;
-    return u_x * v_y - u_y - v_x < 0;
+    return u_x * v_y - u_y * v_x < 0;
 }
 
 int main() {
@@ -17,6 +17,8 @@ int main() {
 
     for (const auto &q : query)
         for (size_t i = 1; i < n - 1; i++)
-            if (cross_product(convex[i - 1], convex[i], q) and cross_product(q, convex[i], convex[i + 1]))
+            if (cross_product(convex[i - 1], convex[i], q) and cross_product(q, convex[i], convex[i + 1])) {
                 cout << i + 1 << endl;
+                break;
+            }
 }
