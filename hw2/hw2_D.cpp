2dce7b91f2eff9684bfb1c586658079c06d1c986
Seann <seanyu0425@gmail.com>
2024-09-27 16:24:42 +0800

    working

--- hw2/hw2_D.cpp
+++ hw2/hw2_D.cpp
@@ -10,8 +10,9 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-auto build_tree(size_t, const vector<size_t> &) -> TreeNode *; // build tree based on the array
-auto search(TreeNode *, size_t &) -> unordered_set<int> *;     // find the number of special segments of the root
+auto build_tree(size_t, const vector<int> &, const vector<size_t> &) -> TreeNode *; // build tree based on the array
+auto search(TreeNode *, size_t &) -> unordered_set<int> *; // find the number of special segments of the root
+vector<TreeNode *> node_pos;
 
 int main() {
     // initialize
@@ -23,28 +24,45 @@ int main() {
         cin >> i;
         pos[static_cast<size_t>(i - 1)] = idx++;
     }
-    TreeNode *root = build_tree(n, pos);
+    TreeNode *root = build_tree(n, nums, pos);
     search(root, ans);
     cout << ans << endl;
     return 0;
 }
 
-auto build_tree(size_t n, const vector<size_t> &pos) -> TreeNode * {
-    TreeNode *root = new TreeNode(static_cast<int>(n)), *cur, *prev;
-    for (size_t i = n - 1; i >= 1; i--) {
-        prev = cur = root;
-        while (cur) {
-            prev = cur;
-            if (pos[i - 1] > pos[static_cast<size_t>(cur->val - 1)]) cur = cur->right;
-            else cur = cur->left;
-        }
-        (pos[i - 1] > pos[static_cast<size_t>(prev->val - 1)] ? prev->right : prev->left) =
-            new TreeNode(static_cast<int>(i));
+auto build_tree(size_t n, const vector<int> &nums, const vector<size_t> &pos) -> TreeNode * {
+    // O(n)
+    vector<int> left_side(n), right_side(n);
+    node_pos.resize(n);
+    stack<int> monotonic;
+
+    // left
+    for (size_t i = 0; i < n; i++) {
+        while (!monotonic.empty() and monotonic.top() < nums[i]) monotonic.pop();
+        left_side[i] = monotonic.empty() ? numeric_limits<int>::max() : monotonic.top();
+        monotonic.push(nums[i]);
     }
-    return root;
+
+    // right
+    monotonic = stack<int>();
+    for (size_t i = n - 1; i < n; i--) {
+        while (!monotonic.empty() and monotonic.top() < nums[i]) monotonic.pop();
+        right_side[i] = monotonic.empty() ? numeric_limits<int>::max() : monotonic.top();
+        monotonic.push(nums[i]);
+    }
+
+    // start building tree
+    node_pos.back() = new TreeNode(static_cast<int>(n));
+    for (size_t i = n - 1; i >= 1; i--)
+        (left_side[pos[i - 1]] < right_side[pos[i - 1]] ? node_pos[left_side[pos[i - 1]] - 1]->right
+                                                        : node_pos[right_side[pos[i - 1]] - 1]->left) =
+            new TreeNode(static_cast<int>(i));
+
+    return node_pos.back();
 }
 
 auto search(TreeNode *root, size_t &ans) -> unordered_set<int> * {
+    // O(nlogn)
     if (!root) {
         unordered_set<int> *leaf = new unordered_set<int>();
         return leaf;
