0201344f4c3652c6aba2a664590db6bab19e74af
Seann <seanyu0425@gmail.com>
2024-09-27 15:05:04 +0800

    tle

--- hw2/hw2_D.cpp
+++ hw2/hw2_D.cpp
@@ -10,14 +10,12 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-auto build_tree(size_t n) -> TreeNode * {
-    TreeNode *root = new TreeNode(static_cast<int>(n)), *cur, *prev;
-    for (int i = static_cast<int>(n - 1); i >= 1; i++) { prev = cur = root; }
-}
+auto build_tree(size_t, const vector<size_t> &) -> TreeNode *; // build tree based on the array
+auto search(TreeNode *, size_t &) -> unordered_set<int> *;     // find the number of special segments of the root
 
 int main() {
     // initialize
-    size_t n, idx = 0;
+    size_t n, idx = 0, ans{};
     cin >> n;
     vector<int> nums(n);
     vector<size_t> pos(n);
@@ -25,5 +23,37 @@ int main() {
         cin >> i;
         pos[static_cast<size_t>(i - 1)] = idx++;
     }
-    TreeNode *root = build_tree(n);
+    TreeNode *root = build_tree(n, pos);
+    search(root, ans);
+    cout << ans << endl;
+    return 0;
+}
+
+auto build_tree(size_t n, const vector<size_t> &pos) -> TreeNode * {
+    TreeNode *root = new TreeNode(static_cast<int>(n)), *cur, *prev;
+    for (size_t i = n - 1; i >= 1; i--) {
+        prev = cur = root;
+        while (cur) {
+            prev = cur;
+            if (pos[i - 1] > pos[static_cast<size_t>(cur->val - 1)]) cur = cur->right;
+            else cur = cur->left;
+        }
+        (pos[i - 1] > pos[static_cast<size_t>(prev->val - 1)] ? prev->right : prev->left) =
+            new TreeNode(static_cast<int>(i));
+    }
+    return root;
+}
+
+auto search(TreeNode *root, size_t &ans) -> unordered_set<int> * {
+    if (!root) {
+        unordered_set<int> *leaf = new unordered_set<int>();
+        return leaf;
+    }
+    auto minimum = search(root->left, ans), maximum = search(root->right, ans);
+    if (minimum->size() > maximum->size()) swap(minimum, maximum);
+    for (const auto &i : *minimum)
+        if (maximum->count(root->val - i)) ans++;
+    maximum->insert(minimum->begin(), minimum->end());
+    maximum->insert(root->val);
+    return maximum;
 }
