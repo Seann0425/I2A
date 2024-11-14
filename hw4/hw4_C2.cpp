37eb21b0fc8bee7aa83194283ffcabf1e201892f
Seann <seanyu0425@gmail.com>
2024-11-14 13:29:58 +0800

    c2 wa

--- hw4/hw4_C2.cpp
+++ hw4/hw4_C2.cpp
@@ -23,9 +23,9 @@ int main() {
     // priority queue to find the previous non-overlapping event
     priority_queue<pair<int, size_t>> last_event;
     previous.resize(n, n); // n if no previous non-overlapping event
-    for (size_t i = n - 1; i < n; i++) {
+    for (size_t i = n - 1; i < n; i--) {
         const auto &[s, e, p] = events[i];
-        while (!last_event.empty() && last_event.top().first > s) {
+        while (!last_event.empty() && last_event.top().first > e) {
             previous[last_event.top().second] = i;
             last_event.pop();
         }
@@ -35,7 +35,16 @@ int main() {
     // dp
     dp.resize(n, 0);
     dp.front() = get<2>(events.front());
-    for (size_t i = n - 1; i < n; i++) { auto [s, e, p] = events[i]; }
+    for (size_t i = n - 1; i < n; i--) {
+        auto [s, e, p] = events[i];
+        dp[i] = max(get_optimal(i - 1), get_optimal(previous[i]) + p);
+    }
+
+    cout << dp.back() << endl;
 }
 
-auto get_optimal(size_t i) -> int {}
+auto get_optimal(size_t i) -> int {
+    if (dp[i]) return dp[i];
+    auto &point = get<2>(events[i]);
+    return dp[i] = (i < dp.size() ? max(get_optimal(i - 1), get_optimal(previous[i]) + point) : point);
+}
